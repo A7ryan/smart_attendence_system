e0f2284d6d7d8f63c5c07f2013363ae67fff0fa5
A7ryan <136580189+A7ryan@users.noreply.github.com>
2025-01-19 21:16:46 +0530

    Update library.py

--- smart_attendence_system/library.py
+++ smart_attendence_system/library.py
@@ -6,12 +6,3 @@ import datetime
 import numpy
 import time
 import mysql.connector
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database = "charusat_students"
-)
-
-mycursor = mydb.cursor()
